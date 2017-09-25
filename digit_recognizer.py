e9eb1c21e47bfff6924cfa416e00840285023a0f
tothanhtung0205 <tothanhtung0205@gmail.com>
2017-09-25 14:52:30 +0700

    add open cv

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -5,6 +5,10 @@ hiddenlayer : 30 units
 outputlayer : 10 units
 '''
 import numpy as np
+import cv2
+from sklearn.externals import joblib
+
+
 def get_data():
     #get data from csv into matrix
     data = np.genfromtxt('train.csv',delimiter=',')
@@ -28,9 +32,9 @@ def random_params_initialize(L_in,L_out):
     epsilon_init = 0.12
     return np.random.uniform(low = -epsilon_init,high=epsilon_init,size=(L_out,L_in+1))
 
-def unroll_matrix(): #unrolling params
-    theta1 = random_params_initialize(784,100)
-    theta2 = random_params_initialize(100,10)
+def unroll_matrix(input,hidden,output): #unrolling params
+    theta1 = random_params_initialize(input,hidden)
+    theta2 = random_params_initialize(hidden,output)
     print "theta1 :",theta1.shape
     print "theta2 :",theta2.shape
     theta = np.concatenate((theta1.flatten(),theta2.flatten()))
@@ -63,39 +67,7 @@ def nn_cost_func(theta,x_train,y_train,inputlayer_size,hidden_layer_size,output_
     J += _lambda * ((theta1[:, 1:] ** 2).sum() + (theta2[:, 1:] ** 2).sum()) / (2 * m)
     return  J
 
-'''
-def gradient_checking(theta,x_train,y_train,input_layer,hidden_layer,output_layer,_lambda):                 #J???
-    checking_grad = np.zeros(theta.shape)
-    exl_matrix = np.zeros(theta.shape)
-    epxilon = 0.0001
-    for i in range(len(theta)):
-        print "theta ", theta
-        exl_matrix[i] = epxilon
-        theta_plus = theta+exl_matrix
-        print "theta plus:",theta_plus
-        theta_minus = theta - exl_matrix
-        print "theta minus:",theta_minus
-        J_theta_plus = nn_cost_func(theta_plus,x_train,y_train,input_layer,hidden_layer,output_layer,_lambda)
-        J_theta_minus = nn_cost_func(theta_minus,x_train,y_train,input_layer,hidden_layer,output_layer,_lambda)
-        checking_grad[i] = (J_theta_plus-J_theta_minus)/(2*epxilon)
-        exl_matrix[i] = 0
-    return checking_grad
-def test_gradient():
-    x_train,y_train = get_data()
-    x_train = x_train[:1000,340:350]
-    y_train = y_train[:1000,:]
-    theta1 = random_params_initialize(10,5,0.12)
-    theta2 = random_params_initialize(5,3,0.12)
-    theta = np.concatenate((theta1.flatten(),theta2.flatten()))
-    _lambda = 3
-    checking_Delta = gradient_checking(theta,x_train,y_train,10,5,3,_lambda)
-    print "checking_Delta:",checking_Delta
-    backpro_Delta = back_propagation(theta, x_train, y_train, 10,5,3, _lambda)
-    print "Backpro Delta:",backpro_Delta
-    diff = np.linalg.norm(checking_Delta - backpro_Delta) / np.linalg.norm(checking_Delta + backpro_Delta)
-    print "diff",diff
-    return diff
-'''
+
 def back_propagation(theta,x_train,y_train,inputlayer_size,hidden_layer_size,output_layer_size,_lambda):
     m = x_train.shape[0]                                            #number of training examples (42000)
     x_train = np.concatenate((np.ones((m, 1)), x_train), axis=1)    #add column with all values = 1 into x_train
@@ -138,23 +110,6 @@ def back_propagation(theta,x_train,y_train,inputlayer_size,hidden_layer_size,out
     return delta_upcase
 
 
-'''
-def gradient_descent(theta,x_train,y_train,inputlayer_size,hidden_layer_size,output_layer_size,_lambda,learning_rate,num_iters):
-    m = x_train.shape[0]
-    J_history = np.zeros(num_iters)
-    delta = back_propagation(theta,x_train,y_train,inputlayer_size,hidden_layer_size,output_layer_size,_lambda)
-    #//TODO delta1 delta2
-    theta1 = theta[0:(hidden_layer * (input_layer + 1))].reshape([hidden_layer, input_layer + 1])
-    theta2 = theta[(hidden_layer * (input_layer + 1)):].reshape([output_layer, hidden_layer + 1])
-    for i in xrange(num_iters):
-        theta1[:,0]-=learning_rate*delta1[:,0]
-        theta1[:,1:]-=learning_rate*(delta1[:,1:] + _lambda*theta1[:,1:]/m)
-        theta2[:,0] -= learning_rate*delta2[:,0]
-        theta2[:,1:] -= learning_rate*(delta2[:,1:]+_lambda*theta2[:,1:]/m)
-        last_theta = np.concatenate((theta1.flatten(),theta2.flatten()))
-        J_history[i] = nn_cost_func(last_theta,x_train,y_train,inputlayer_size,hidden_layer_size,output_layer_size,_lambda)
-    return  theta, J_history
-'''
 def predict(theta1,theta2,x_test):
     m = x_test.shape[0]
     x_test = np.concatenate((np.ones((m, 1)), x_test), axis=1)
@@ -172,43 +127,66 @@ def get_test_data():
     print x_test.shape
     return x_test
 
+x = get_test_data()
+pass
+
+def fit(model):
+    input_layer = 784
+    hidden_layer = 50
+    output_layer = 10
+    _lambda = 1
+
+    try:
+        theta = joblib.load(model)
+        print("Load params completed!")
+    except:
+        print("getting data.....")
+        x_train,y_train = get_data()
+        initial_theta1 = random_params_initialize(input_layer,hidden_layer)
+        initial_theta2 = random_params_initialize(hidden_layer,output_layer)
+        initial_theta = np.concatenate((initial_theta1.flatten(),initial_theta2.flatten()))
+
+        from scipy.optimize import fmin_l_bfgs_b
+        print 'Training Neural Network...'
+        theta, f, d = fmin_l_bfgs_b(nn_cost_func,initial_theta,
+                                  fprime=back_propagation,
+                                  args=(x_train, y_train,input_layer,
+                                        hidden_layer,
+                                        output_layer,
+                                         _lambda),
+                                  disp=False, maxiter=100)
+        #theta,J_history = gradient_descent(initial_theta,x_train,y_train,input_layer,hidden_layer,output_layer,_lambda,0.001,50)
+        #print J_history
+        joblib.dump(theta,model)
+
+    theta1 = theta[0:(hidden_layer * (input_layer + 1))].reshape([hidden_layer, input_layer + 1])
+    theta2 = theta[(hidden_layer * (input_layer + 1)):].reshape([output_layer, hidden_layer + 1])
+
+    return theta1,theta2
+
+theta1,theta2 = fit('ann_.pkl')
 
 
 
+def predict_image(image):
+    img_test = cv2.img = cv2.imread(image,0)
+    img_test = cv2.resize(img_test,(28,28))
+    vector = img_test.flatten()
+
+    vector = map(lambda x: 255 - x, vector)
+    print("Predict image.")
+    x = np.array([vector])
+    print predict(theta1,theta2,x)
 
 
+#predict_image('mnist/2.png')
 
 
-x_train,y_train = get_data()
-input_layer = 784
-hidden_layer = 100
-output_layer = 10
-_lambda = 0.5
-
-initial_theta1 = random_params_initialize(input_layer,hidden_layer)
-initial_theta2 = random_params_initialize(hidden_layer,output_layer)
-initial_theta = np.concatenate((initial_theta1.flatten(),initial_theta2.flatten()))
-
-from scipy.optimize import fmin_l_bfgs_b
-print 'Training Neural Network...'
-theta, f, d = fmin_l_bfgs_b(nn_cost_func,initial_theta,
-                          fprime=back_propagation,
-                          args=(x_train, y_train,input_layer,
-                                hidden_layer,
-                                output_layer,
-                                 _lambda),
-                          disp=False, maxiter=100)
-
-#theta,J_history = gradient_descent(initial_theta,x_train,y_train,input_layer,hidden_layer,output_layer,_lambda,0.001,50)
-#print J_history
-theta1 = theta[0:(hidden_layer * (input_layer + 1))].reshape([hidden_layer, input_layer + 1])
-theta2 = theta[(hidden_layer * (input_layer + 1)):].reshape([output_layer, hidden_layer + 1])
 x_test = get_test_data()
 predict_nn = predict(theta1,theta2,x_test)
+print("Predict nn .....")
+print predict_nn
 size_test = predict_nn.shape[0]
-print size_test
-print "predict values:",predict_nn[0:100]
-
 imageId = np.array(range(1,predict_nn.shape[0]+1)).reshape(predict_nn.shape[0],1)
 data = np.concatenate((imageId,predict_nn),axis = 1)
 data = data.astype(int)
@@ -220,6 +198,66 @@ with open("resultTR.csv", "wb") as f:
 
 
 
+
+
+
+
+
+
+
+'''
+def gradient_checking(theta,x_train,y_train,input_layer,hidden_layer,output_layer,_lambda):                 #J???
+    checking_grad = np.zeros(theta.shape)
+    exl_matrix = np.zeros(theta.shape)
+    epxilon = 0.0001
+    for i in range(len(theta)):
+        print "theta ", theta
+        exl_matrix[i] = epxilon
+        theta_plus = theta+exl_matrix
+        print "theta plus:",theta_plus
+        theta_minus = theta - exl_matrix
+        print "theta minus:",theta_minus
+        J_theta_plus = nn_cost_func(theta_plus,x_train,y_train,input_layer,hidden_layer,output_layer,_lambda)
+        J_theta_minus = nn_cost_func(theta_minus,x_train,y_train,input_layer,hidden_layer,output_layer,_lambda)
+        checking_grad[i] = (J_theta_plus-J_theta_minus)/(2*epxilon)
+        exl_matrix[i] = 0
+    return checking_grad
+def test_gradient():
+    x_train,y_train = get_data()
+    x_train = x_train[:1000,340:350]
+    y_train = y_train[:1000,:]
+    theta1 = random_params_initialize(10,5,0.12)
+    theta2 = random_params_initialize(5,3,0.12)
+    theta = np.concatenate((theta1.flatten(),theta2.flatten()))
+    _lambda = 3
+    checking_Delta = gradient_checking(theta,x_train,y_train,10,5,3,_lambda)
+    print "checking_Delta:",checking_Delta
+    backpro_Delta = back_propagation(theta, x_train, y_train, 10,5,3, _lambda)
+    print "Backpro Delta:",backpro_Delta
+    diff = np.linalg.norm(checking_Delta - backpro_Delta) / np.linalg.norm(checking_Delta + backpro_Delta)
+    print "diff",diff
+    return diff
+'''
+
+
+
+'''
+def gradient_descent(theta,x_train,y_train,inputlayer_size,hidden_layer_size,output_layer_size,_lambda,learning_rate,num_iters):
+    m = x_train.shape[0]
+    J_history = np.zeros(num_iters)
+    delta = back_propagation(theta,x_train,y_train,inputlayer_size,hidden_layer_size,output_layer_size,_lambda)
+    #//TODO delta1 delta2
+    theta1 = theta[0:(hidden_layer * (input_layer + 1))].reshape([hidden_layer, input_layer + 1])
+    theta2 = theta[(hidden_layer * (input_layer + 1)):].reshape([output_layer, hidden_layer + 1])
+    for i in xrange(num_iters):
+        theta1[:,0]-=learning_rate*delta1[:,0]
+        theta1[:,1:]-=learning_rate*(delta1[:,1:] + _lambda*theta1[:,1:]/m)
+        theta2[:,0] -= learning_rate*delta2[:,0]
+        theta2[:,1:] -= learning_rate*(delta2[:,1:]+_lambda*theta2[:,1:]/m)
+        last_theta = np.concatenate((theta1.flatten(),theta2.flatten()))
+        J_history[i] = nn_cost_func(last_theta,x_train,y_train,inputlayer_size,hidden_layer_size,output_layer_size,_lambda)
+    return  theta, J_history
+'''
 
 
 
